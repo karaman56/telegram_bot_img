461527f63c5c2eda6cd16a45ce5e6c22260789c4
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-10-23 20:20:48 +0500

    Исправил load_dotenv()
    Обернул в код в ifmain/ def main load_dotenv()

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 from dotenv import load_dotenv
@@ -8,19 +7,13 @@ from download_apod import download_apod_images
 from download_epic import download_epic_images
 from download_spacex import download_spacex_image
 
+
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
-NASA_API_KEY = os.getenv('NASA_API_KEY')
-PUBLISH_INTERVAL = int(os.getenv('PUBLISH_INTERVAL'))
-APOD_IMAGES_DIRECTORY = os.getenv('APOD_IMAGES_DIRECTORY')
-EPIC_IMAGES_DIRECTORY = os.getenv('EPIC_IMAGES_DIRECTORY')
-SPACEX_IMAGES_DIRECTORY = './spacex_images'
 
-def start_image_fetcher():
+def start_image_fetcher(config):
     """Запускает процесс получения изображений и их публикации в Telegram."""
-    telegram_bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    telegram_bot = Bot(token=config['telegram_bot_token'])
 
     while True:
         print("Скачивание изображений APOD...")
@@ -32,13 +25,20 @@ def start_image_fetcher():
         print("Скачивание изображения SpaceX...")
         download_spacex_image()
 
+        publish_images(telegram_bot, config['apod_images_directory'])
+        publish_images(telegram_bot, config['epic_images_directory'])
+        publish_images(telegram_bot, config['spaces_images_directory'])
 
-        publish_images(telegram_bot, './apod_images')
-        publish_images(telegram_bot, './epic_images')
-        publish_images(telegram_bot, './spacex_images')
+        print(f"Ожидание следующего обновления через {config['publish_interval']} секунд...")
+        time.sleep(config['publish_interval'])
 
-        print(f"Ожидание следующего обновления через {PUBLISH_INTERVAL} секунд...")
-        time.sleep(PUBLISH_INTERVAL)
 
 if __name__ == "__main__":
-    start_image_fetcher()
+    "telegram_bot_token": os.getenv('TELEGRAM_BOT_TOKEN'),
+    "telegram_channel_id": os.getenv('TELEGRAM_CHANNEL_ID'),
+    "nasa_api_key": os.getenv('NASA_API_KEY'),
+    "publish_interval": int(os.getenv('PUBLISH_INTERVAL')),
+    "apod_images_directory": os.getenv('APOD_IMAGES_DIRECTORY'),
+    "epic_images_directory": os.getenv('EPIC_IMAGES_DIRECTORY'),
+    "spaces_images_directory": './spacex_images'
+    start_image_fetcher(config)
