0b577a33e9788cb340316c7be48d41ce157a266e
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-11-02 21:42:06 +0500

    Добавил параметры Get
    Добавил параметры Get

--- apod.py
+++ apod.py
@@ -6,14 +6,12 @@ from dotenv import load_dotenv
 
 APOD_IMAGES_DIRECTORY = "./apod_images"
 
-
 def get_apod_image(api_key):
     params = {'api_key': api_key}
     url = 'https://api.nasa.gov/planetary/apod?' + urlencode(params)
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
-
 
 def save_image(image_url, directory, prefix):
     image_response = requests.get(image_url)
@@ -26,12 +24,11 @@ def save_image(image_url, directory, prefix):
         file.write(image_response.content)
     return file_path
 
-
 def download_apod_images(count=1, api_key=None):
     os.makedirs(APOD_IMAGES_DIRECTORY, exist_ok=True)
     for _ in range(count):
         apod_image_save = get_apod_image(api_key)
-        if apod_image_save and 'url' in apod_image_save:
+        if apod_image_save and 'url' in apod_image_data:
             image_url = apod_image_save['url']
             print(f"APOD Image URL: {image_url}")
             file_path = save_image(image_url, APOD_IMAGES_DIRECTORY, "apod")
@@ -40,11 +37,12 @@ def download_apod_images(count=1, api_key=None):
         else:
             print("Нет данных для APOD.")
 
-
 if __name__ == "__main__":
     load_dotenv()
     api_key_nasa = os.getenv('NASA_API_KEY')
     download_apod_images(count=3, api_key=api_key_nasa)
+
+
 
 
 
