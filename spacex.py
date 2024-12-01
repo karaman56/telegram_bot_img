973a96b602e367fd6ec7347ae7f4f4014f8570a8
karaman56 <166434324+karaman56@users.noreply.github.com>
2024-12-02 02:40:15 +0500

    Update spacex.py
    Update spacex.py

--- spacex.py
+++ spacex.py
@@ -1,34 +1,29 @@
 import requests
-from urllib.parse import urlencode
 from download_tools import save_image
 
 
-def get_the_latest_launch_photo(api_key=None):
+def get_the_latest_launch_photo():
     """Получает информацию о последнем запуске SpaceX."""
     url = 'https://api.spacexdata.com/v4/launches/latest'
-    params = {
-        'api_key': api_key
-    }
-    response = requests.get(url, params=params)
+    response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
-def extract_image_url(launch_data):
-    """Извлекает URL изображения из данных о запуске."""
-    return launch_data["links"]["patch"]["large"]
 
-def download_spacex_image():
-    """Скачивает изображение последнего запуска SpaceX и возвращает его содержимое в виде байтов."""
+def main():
+    """Основная функция программы."""
     launch_data = get_the_latest_launch_photo()
-    image_url = extract_image_url(launch_data)
+    image_url = launch_data["links"]["patch"]["large"]
 
     if image_url:
-        return save_image(image_url)
-    return None
+        image_bytes = save_image(image_url)
+       
 
 
 if __name__ == "__main__":
-    image_bytes = download_spacex_image()
+    main()
+
+
 
     
 
